28338fee5f738d712ac24c1fab7ae790882b11b0
Siddhant Jaiswal <siddhantjaiswal.mst18@iitbhu.ac.in>
2022-01-05 00:08:52 +0530

    slider added for films and series

--- js/browse.js
+++ js/browse.js
@@ -3,7 +3,6 @@ jQuery(document).ready(function () {
   jQuery(".browser-slider").slick({
     slidesToShow: 5,
     arrows: false,
-    // centerMode: true,
     responsive: [
       {
         breakpoint: 460,
@@ -17,24 +16,23 @@ jQuery(document).ready(function () {
 
   // Custom Tabber
   jQuery("#series").click(function () {
-    console.log('series');
+    // console.log("series");
     // jQuery(this).addClass("series-showcase");
     // jQuery("#films").removeClass("films-showcase");
-    jQuery('.showcase').addClass('series-banner');
+    jQuery(".showcase").addClass("series-banner");
     jQuery(".series-showcase").show();
     jQuery(".films-showcase").hide();
     jQuery(".series-section").show();
     jQuery(".films-section").hide();
   });
 
-    jQuery("#films").click(function () {
+  jQuery("#films").click(function () {
     // jQuery(this).addClass("films-showcase");
     // jQuery("#series").removeClass("series-showcase");
-    jQuery('.showcase').removeClass('series-banner');
+    jQuery(".showcase").removeClass("series-banner");
     jQuery(".series-showcase").hide();
     jQuery(".films-showcase").show();
     jQuery(".series-section").hide();
     jQuery(".films-section").show();
   });
-
 });
